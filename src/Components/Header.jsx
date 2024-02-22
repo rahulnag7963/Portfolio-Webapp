42dc5101ae70447b78b40ddddd7ea3f092e78909
Rahul <rahulnag7963@gmail.com>
2024-02-22 02:27:32 -0800

    More changes for mobile view

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Nav, Container} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
 import logo from "../Assets/logo2.png";
 import {BrowserRouter as Router, Link} from "react-router-dom";
 import { motion } from "framer-motion"
@@ -24,10 +24,35 @@ const Header = () => {
             alt="Rahul's logo"
           />
           </motion.button>
+              <a className="" href="https://www.linkedin.com/in/rahul-nagulapally-8250a5196/">
+                <motion.img
+                className="btn btn-light" 
+                whileHover={{ 
+                  scale: 1.1,
+                  boxShadow: "10px 10px 8px rgba(84,180,211,1)"
+                }}
+                src="https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png" 
+                alt=""
+                width="60"
+                height="50"
+                />
+              </a>
+              <a className="" href="https://github.com/rahulnag7963">
+                <motion.img
+                className="btn btn-light"
+                whileHover={{ 
+                  scale: 1.1,
+                  boxShadow: "10px 10px 8px rgba(84,180,211,1)"
+                }} 
+                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+                alt="" 
+                width="60"
+                height="50"
+                />
+              </a>  
           </Nav.Link>  
         </Navbar.Brand>
-        <Container>
-          <Navbar.Collapse className="justify-content-end">
+          <Navbar.Collapse className="m-4">
             <Nav>
               <Nav.Link as={Link} to = "/Skills">
                 <motion.div
@@ -85,36 +110,7 @@ const Header = () => {
                 </motion.div>
               </Nav.Link>
             </Nav>
-            <div>
-              <a className="" href="https://www.linkedin.com/in/rahul-nagulapally-8250a5196/">
-                <motion.img
-                className="btn btn-light" 
-                whileHover={{ 
-                  scale: 1.1,
-                  boxShadow: "10px 10px 8px rgba(84,180,211,1)"
-                }}
-                src="https://static-00.iconduck.com/assets.00/linkedin-icon-2048x2048-ya5g47j2.png" 
-                alt=""
-                width="60"
-                height="50"
-                />
-              </a>
-              <a className="" href="https://github.com/rahulnag7963">
-                <motion.img
-                className="btn btn-light"
-                whileHover={{ 
-                  scale: 1.1,
-                  boxShadow: "10px 10px 8px rgba(84,180,211,1)"
-                }} 
-                src="https://cdn-icons-png.flaticon.com/512/25/25231.png"
-                alt="" 
-                width="60"
-                height="50"
-                />
-              </a>
-            </div>   
           </Navbar.Collapse>
-        </Container>
       </Navbar>
       <AnimatedRoutes/>
     </Router>
